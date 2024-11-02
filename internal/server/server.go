91a505ca470208139a28d2f35eab9df13f163e6e
Abhay Chandavar <abhaychandavar11@gmail.com>
2024-11-02 23:42:16 +0530

    Initial commit

---
+++ internal/server/server.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"auth/internal/database"
+)
+
+type FiberServer struct {
+	*fiber.App
+
+	db database.Service
+}
+
+func New() *FiberServer {
+	server := &FiberServer{
+		App: fiber.New(fiber.Config{
+			ServerHeader: "auth",
+			AppName:      "auth",
+		}),
+
+		db: database.New(),
+	}
+
+	return server
+}
